c9c4738d61bf51c750939dbea8e344648c7229c9
Meshari Almoteriy <fy222ma@leeds.ac.uk>
2026-08-20 10:01:58 +0300

    Added reset input, for it to start at a known location

--- trigger.v
+++ trigger.v
@@ -2,6 +2,7 @@ module trigger (
     input  clk,             // Sample clock — this module reacts once per tick
     input  probe_in,  	 // The (synced) signal we're watching for an edge
 	 input polarity,
+	 input reset,
     output reg output_trigger  // Fires HIGH for exactly one tick when the edge happens
 );
 
@@ -11,13 +12,19 @@ module trigger (
                         // nothing outside this module ever needs to see it —
                         // it's private, internal memory only.
 
- always @(posedge clk) begin
-    if (polarity) begin
+ always @(posedge clk or posedge reset) begin
+    if (reset) begin 
+	 output_trigger <= 0;
+	 prev_sample    <=0;
+	 end else 
+	 if (polarity) begin
         // polarity = 1 means "watch for rising edges"
         if ((!prev_sample) && probe_in)
             output_trigger <= 1;
         else
             output_trigger <= 0;
+				prev_sample <= probe_in;     
+
     end else begin
         // polarity = 0 means "watch for falling edges"
         if (prev_sample&&(!probe_in))  
@@ -25,9 +32,10 @@ module trigger (
             output_trigger <= 1;
         else
             output_trigger <= 0;
+				prev_sample <= probe_in;      // moved in here
+
     end
 
-    prev_sample <= probe_in;   // this still runs every tick, no matter what
 end
       
  
